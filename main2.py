29687127a3078c602a590dbf9b34efb983304f0b
Elanchezhiya <50616407+Elanchezhiya@users.noreply.github.com>
2019-05-14 16:16:58 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,10 @@
+a=str(input())
+mylist = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+a.lower()
+if(a in mylist):
+ print("Alphabet")
+else:
+ print("No")
+
+
+
