74e6618cc8cf95340e1a23807fab828774e2f4f2
Elanchezhiya <50616407+Elanchezhiya@users.noreply.github.com>
2019-05-18 14:03:52 +0530

    Update lpyr.py

--- lpyr.py
+++ lpyr.py
@@ -1,11 +1,5 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: C:/Users/Elanchezhiyan/AppData/Local/Programs/Python/Python37-32/leapyr.py 
-Enter year to be checked:2013
-no
->>> 
- RESTART: C:/Users/Elanchezhiyan/AppData/Local/Programs/Python/Python37-32/leapyr.py 
-2012
-yes
->>> p
+year=int(input())
+if(year%4==0 and year%100!=0 or year%400==0):
+    print("yes")
+else:
+    print("no")
