720974837b3aae0efd53dcf9b4983335db074acf
Elanchezhiya <50616407+Elanchezhiya@users.noreply.github.com>
2020-01-06 20:39:29 +0530

    Create hhh.java

---
+++ hhh.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      int n,i;
+        Scanner sc= new Scanner(System.in);
+        n=sc.nextInt();
+       for(i=1;i<=n;i++)
+       {
+           n=i*i+1;
+       
+        System.out.print(n+" ");
+  
+       }
+    
+}}
+
